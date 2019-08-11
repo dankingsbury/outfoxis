0661252f9515169d35bdbb3d698f7a03de4baa06
Dan Kingsbury <dkingsbury@outfoxis.com>
2019-08-11 00:33:08 -0400

    Implement webpack minifcation workaround

--- functions-src/hello.js
+++ functions-src/hello.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 exports.handler = function(event, context, callback) {
     callback(null, {
-    statusCode: 200,
-    body: "Hello, World " + (new Date()).toString()
+        statusCode: 200,
+        body: "Hello, World " + (new Date()).toString()
     });
 }
